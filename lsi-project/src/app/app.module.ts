938ddf4c115525b52edefb24cde27237e766a5e8
Aleksandra Ziobrowska <olaziobrowska1@gmail.com>
2022-08-10 18:25:52 +0200

    feat: wip

--- lsi-project/src/app/app.module.ts
+++ lsi-project/src/app/app.module.ts
@@ -1,8 +1,12 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent, ProductService} from './app.component';
+
+import {CalendarModule} from 'primeng/calendar';
+import {TableModule} from "primeng/table";
+import {HttpClientModule} from "@angular/common/http";
 
 @NgModule({
   declarations: [
@@ -10,9 +14,13 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    CalendarModule,
+    TableModule,
+    HttpClientModule,
   ],
-  providers: [],
+  providers: [ProductService],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
